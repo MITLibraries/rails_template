4d63f512530db97f19c6f4dd23de82761109b1c8
Jeremy Prevost <jprevost@mit.edu>
2016-06-20 14:31:35 -0400

    Switch to Puma and address gem changes
    - This uses Puma instead of Passenger
    - Some gems now have some different syntax so this addresses some
    deprecation warnings.

--- template.rb
+++ template.rb
@@ -6,7 +6,7 @@ def apply_template
 
   copy_file 'Gemfile.tt', 'Gemfile', force: true
 
-  create_file '.ruby-version', '2.2.3'
+  create_file '.ruby-version', '2.3.1'
   copy_file '.rubocop.yml'
   copy_file 'LICENSE.md'
 
@@ -21,7 +21,7 @@ def apply_template
   copy_file 'app/controllers/users/omniauth_callbacks_controller.rb'
 
   create_file 'Procfile' do
-    'web: bundle exec passenger start -p $PORT --max-pool-size 3'
+    'web: bundle exec puma -C config/puma.rb'
   end
 
   gsub_file 'Rakefile', /will automatically be/, 'will be'
@@ -67,6 +67,10 @@ end
 
 def add_routes
   copy_file 'config/routes.rb', force: true
+end
+
+def add_puma
+  copy_file 'config/puma.rb', force: true
 end
 
 def add_tests
